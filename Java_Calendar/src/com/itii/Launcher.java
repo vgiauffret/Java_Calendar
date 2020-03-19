eb3f08227fb464752d2d6e232b8c6554d8875e32
malik <bedouimalik@gmail.com>
2020-03-19 12:53:39 +0100

    Ajout de la classe connexion pour écrire dans la base de donnée les tâches correspondantes

--- Java_Calendar/src/com/itii/Launcher.java
+++ Java_Calendar/src/com/itii/Launcher.java
@@ -1,13 +1,16 @@
 package com.itii;
+import java.sql.SQLException;
+
 import javax.swing.*;
+import com.itii.db.*;
+
 
 public class Launcher {
-    public static void main(String[] args) {
+	
+   public static void main(String[] args) throws SQLException {
+    	
+    	 MainWindow.getInstance();
 
-        MainWindow.getInstance();
-
-
-
-
+    	 
     }
 }
